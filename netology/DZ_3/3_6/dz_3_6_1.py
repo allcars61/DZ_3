563565b3a6b6ac9d46cb5cc965a8697ac71b8dc0
Aleksandr Tolkanev <allcars61@gmail.com>
2023-04-27 11:57:30 +0300

    Update dz_3_6_1.py

--- netology/DZ_3/3_6/dz_3_6_1.py
+++ netology/DZ_3/3_6/dz_3_6_1.py
@@ -1,41 +1,69 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Float, DateTime
-from sqlalchemy.ext.declarative import declarative_base
+from engine import engine, Session
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, DateTime
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
+
 class Publisher(Base):
-    __tablename__ = 'publisher'
+    __tablename__ = "publisher"
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
 
+
 class Book(Base):
-    __tablename__ = 'book'
+    __tablename__ = "book"
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
-    publisher_id = Column(Integer, ForeignKey('publisher.id'), nullable=False)
+    publisher_id = Column(Integer, ForeignKey("publisher.id"), nullable=False)
+    publisher = relationship(Publisher, backref="books")
 
 
 class Stock(Base):
-    __tablename__ = 'stock'
+    __tablename__ = "stock"
     id = Column(Integer, primary_key=True)
-    book_id = Column(Integer, ForeignKey('book.id'), nullable=False)
-    shop_id = Column(Integer, ForeignKey('shop.id'), nullable=False)
+    book_id = Column(Integer, ForeignKey("book.id"), nullable=False)
+    book = relationship(Book, backref="stock")
+    shop_id = Column(Integer, ForeignKey("shop.id"), nullable=False)
     count = Column(Integer, nullable=False)
 
+
 class Sale(Base):
-    __tablename__ = 'sale'
+    __tablename__ = "sale"
     id = Column(Integer, primary_key=True)
     price = Column(Float, nullable=False)
     date_sale = Column(DateTime, nullable=False)
-    stock_id = Column(Integer, ForeignKey('stock.id'), nullable=False)
+    stock_id = Column(Integer, ForeignKey("stock.id"), nullable=False)
+    stock = relationship(Stock, backref="sale")
     count = Column(Integer, nullable=False)
 
+
 class Shop(Base):
-    __tablename__ = 'shop'
+    __tablename__ = "shop"
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
 
-if __name__ == '__main__':
-    from engine import engine
+
+if __name__ == "__main__":
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
+
+    publisher_name = input("Введите имя или идентификатор издателя: ")
+
+    sales = (
+        Session()
+        .query(Sale)
+        .join(Stock)
+        .join(Book)
+        .join(Publisher)
+        .filter(Publisher.name == publisher_name)
+        .order_by(Sale.date_sale)
+        .all()
+    )
+
+    for sale in sales:
+        print(
+            f"{sale.stock.book.title} | {sale.stock.shop.name} | {sale.price} | {sale.date_sale}"
+        )
+
