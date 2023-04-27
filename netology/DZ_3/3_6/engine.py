c45135662c821760e21588962b9b49d2ce984907
Aleksandr Tolkanev <allcars61@gmail.com>
2023-04-27 11:57:50 +0300

    Update engine.py

--- netology/DZ_3/3_6/engine.py
+++ netology/DZ_3/3_6/engine.py
@@ -1,9 +1,6 @@
 from sqlalchemy import create_engine
-
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
-engine = create_engine(DATABASE_URL)
-
 from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
+engine = create_engine(DATABASE_URL, echo=False)
 Session = sessionmaker(bind=engine)
-db = Session()
